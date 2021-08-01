02ff44c227cf01cb2d43d03313caa9212c89df12
Code_Junior <46490572+essienmichael4@users.noreply.github.com>
2021-08-01 16:47:05 +0000

    login and signup patched

--- app/src/main/java/com/example/choirapp/LoginActivity.kt
+++ app/src/main/java/com/example/choirapp/LoginActivity.kt
@@ -3,14 +3,25 @@ package com.example.choirapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.util.Patterns
 import android.view.LayoutInflater
+import android.view.View
 import com.example.choirapp.databinding.ActivityLoginBinding
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
+    private lateinit var email:String
+    private lateinit var password:String
+
+
+    private lateinit var auth:FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        auth = FirebaseAuth.getInstance()
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -19,5 +30,65 @@ class LoginActivity : AppCompatActivity() {
             val intent = Intent(this, SignupActivity::class.java)
             startActivity(intent)
         }
+
+        binding.loginUser.setOnClickListener{
+            if (isFormValidated()) {
+                login(email, password, it)
+
+            }
+        }
+    }
+
+    private fun login(email:String,password:String,view: View){
+        widgetVisibility(false,View.VISIBLE)
+        auth.signInWithEmailAndPassword(email, password).
+                addOnCompleteListener { task ->
+                    if (task.isSuccessful){
+                        val intent = Intent(this, AdminActivity::class.java)
+                        startActivity(intent)
+                    }
+                    else{
+                        Log.e("LoginActivity", "${task.exception}")
+                    }
+                }.addOnFailureListener {
+                    widgetVisibility(true, View.GONE)
+                    Snackbar.make(view, "Login failed, please try again", Snackbar.LENGTH_LONG).
+                    apply {
+                        animationMode =  Snackbar.ANIMATION_MODE_SLIDE
+                    }.show()
+        }
+
+    }
+
+    private fun isFormValidated():Boolean{
+        var isValid = true
+        if(binding.emailLogin.text?.isEmpty() == true) {
+            binding.textInputLayout2.error = "Field cannot be empty"
+            isValid = false
+        } else{
+            email = binding.emailLogin.text.toString()
+        }
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(binding.emailLogin.text.toString()).matches()) {
+            binding.textInputLayout2.error = "Enter a valid Email"
+        } else{
+            email = binding.emailLogin.text.toString()
+        }
+
+        if(binding.pwdLogin.text?.isEmpty() == true) {
+            binding.textInputLayout4.error = "Field cannot be empty"
+            isValid = false
+        } else{
+            password = binding.pwdLogin.text.toString()
+        }
+
+        return isValid
+    }
+
+    private fun widgetVisibility(state: Boolean, visible: Int) {
+        binding.loginUser.isEnabled = state
+        binding.loginAdmin.isEnabled = state
+        binding.progressLogin.visibility = visible
+
     }
 }
--- app/src/main/java/com/example/choirapp/SignupActivity.kt
+++ app/src/main/java/com/example/choirapp/SignupActivity.kt
@@ -4,13 +4,21 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.util.Patterns
+import android.view.View
 import com.example.choirapp.databinding.ActivitySignupBinding
+import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
+
+private val TAG = SignupActivity::class.java.name
 
 class SignupActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySignupBinding
     private lateinit var auth: FirebaseAuth
 
+    private lateinit var email: String
+    private lateinit var password: String
+//    private lateinit var name: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivitySignupBinding.inflate(layoutInflater)
@@ -18,25 +26,72 @@ class SignupActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        binding.signupUser.setOnClickListener{
-            signup(binding.emailSignup.text.toString(), binding.pwdSignup.text.toString())
-        }
+        binding.signupUser.setOnClickListener {
+            if (isFormValidated()) {
+                signup(email, password, it)
 
+            }
+        }
     }
 
-    private fun signup(email:String, password:String){
-        
+    private fun signup(email:String, password:String, view: View){
+        widgetVisibility(false, View.VISIBLE)
 
         auth.createUserWithEmailAndPassword(email, password).
-                addOnCompleteListener { task ->
+            addOnCompleteListener { task ->
                     if (task.isSuccessful){
                         val intent = Intent(this, LoginActivity::class.java)
                         startActivity(intent)
-                        finish()
                     }
                     else{
                         Log.e("SignupActivity", "${task.exception}")
                     }
-                }
+            }.addOnFailureListener {
+                widgetVisibility(true, View.GONE)
+                Log.e(TAG, "${it.message}")
+
+                Snackbar.make(view, "Signup failed, please try again", Snackbar.LENGTH_LONG).
+                        apply {
+                            animationMode =  Snackbar.ANIMATION_MODE_SLIDE
+                        }.show()
+            }
+    }
+
+    private fun isFormValidated():Boolean{
+        var isValid = true
+        if(binding.emailSignup.text?.isEmpty() == true) {
+            binding.textInputLayout2.error = "Field cannot be empty"
+            isValid = false
+        } else{
+            email = binding.emailSignup.text.toString()
+        }
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(binding.emailSignup.text.toString()).matches()) {
+            binding.textInputLayout2.error = "Enter a valid Email"
+        } else{
+            email = binding.emailSignup.text.toString()
+        }
+
+        if(binding.pwdSignup.text?.isEmpty() == true) {
+            binding.textInputLayout4.error = "Field cannot be empty"
+            isValid = false
+        } else{
+            password = binding.pwdSignup.text.toString()
+        }
+
+//        if(binding.fullName.text?.isEmpty() == true) {
+//            binding.textInputLayout4.error = "Field cannot be empty"
+//            isValid = false
+//        } else{
+//            name = binding.fullName.text.toString()
+//        }
+
+        return isValid
+    }
+
+
+    private fun widgetVisibility(state: Boolean, visibility: Int) {
+        binding.signupUser.isEnabled = state
+        binding.progressSignUp.visibility = visibility
     }
 }
