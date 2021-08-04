b788d85cf7ddc1d1eb495f2cc08c47c824c2b16d
Code_Junior <46490572+essienmichael4@users.noreply.github.com>
2021-08-04 17:25:46 +0000

    new commit

--- app/src/main/java/com/example/choirapp/AdminActivity.kt
+++ app/src/main/java/com/example/choirapp/AdminActivity.kt
@@ -2,9 +2,14 @@ package com.example.choirapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.choirapp.databinding.ActivityAdminBinding
+import com.example.choirapp.databinding.ActivityLoginBinding
 
 class AdminActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAdminBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityAdminBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_admin)
     }
